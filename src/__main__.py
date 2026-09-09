41ccddb903b626dc3079881b625d53f8ef0e4c1b
Mikhail Peskov <m.peskovds@gmail.com>
2026-09-09 15:30:17 +0200

    checkpoint

--- src/__main__.py
+++ src/__main__.py
@@ -1,11 +1,11 @@
 from models import *
-from index import read_file, chunk_md
+from index import read_file, chunk_md, read_py
+import ast
 
 
 MAX_CHUNK_SIZE = 2000
 
 if __name__ == "__main__":
-    filepath = "./data/raw/vllm-0.10.1/README.md"
-    line = read_file(filepath)
-    minimal_sources = chunk_md(line, filepath)
-    print(minimal_sources[0])
+    filepath = "./src/__main__.py"
+    tree = read_py(filepath)
+    print(ast.dump(tree))
